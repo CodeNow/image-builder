472593ca3a73d156535ecfa0c2f05a06a28e4b50
Bryan Kendall <bryan@runnable.com>
2014-12-09 16:55:15 -0800

    adding a failing test

---
+++ scripts/test-circle-05.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# a test with the cache, but no initial directory
+set -e
+
+test_num="05"
+full_repo="bkendall/flaming-octo-nemesis"
+
+mkdir ./test-"$test_num"
+
+docker run \
+  -e RUNNABLE_AWS_ACCESS_KEY="$AWS_ACCESS_KEY" \
+  -e RUNNABLE_AWS_SECRET_KEY="$AWS_SECRET_KEY" \
+  -e RUNNABLE_FILES_BUCKET='runnable.image-builder' \
+  -e RUNNABLE_PREFIX='' \
+  -e RUNNABLE_FILES='{ "Dockerfile": "K6cluDupwQdFRsuTPJ0SFUrxUB4lmF_Q" }' \
+  -e RUNNABLE_KEYS_BUCKET='runnable.image-builder' \
+  -e RUNNABLE_DEPLOYKEY='flaming-octo-nemesis.key' \
+  -e RUNNABLE_REPO='git@github.com:bkendall/flaming-octo-nemesis' \
+  -e RUNNABLE_COMMITISH='master' \
+  -e RUNNABLE_DOCKER="$(cat DOCKER_IP):5354" \
+  -e RUNNABLE_DOCKERTAG='test-built-image' \
+  -e RUNNABLE_DOCKER_BUILDOPTIONS='' \
+  -v `pwd`/test-"$test_num":/cache:rw \
+  test-image-builder
+
+# it should not be locked
+test ! -d ./test-"$test_num"/"$full_repo".lock
+# the repo should exist
+test -e ./test-"$test_num"/"$full_repo"
+# and the repo should not be cloned _in the cache_
+test -f ./test-"$test_num"/"$full_repo"/README.md
