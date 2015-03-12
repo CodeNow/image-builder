a14be661ff812a8d820988df52114918ce48c976
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-03-11 19:10:11 -0700

    fix nock, use new force flag

--- test/external/signal-client.js
+++ test/external/signal-client.js
@@ -5,14 +5,19 @@ var lab = exports.lab = Lab.script();
 var expect = require('code').expect;
 var it = lab.test;
 var nock = require('nock');
+nock.enableNetConnect();
 
 var Signal = require('../../lib/external/signal-client.js');
 
 lab.experiment('signal-client.js', function () {
   lab.experiment('attachHostToContainer', function () {
+    lab.afterEach(function(done){
+      nock.cleanAll();
+      done();
+    });
     it('should send SIGINT to container', function(done) {
       var signal = new Signal(1,1);
-      nock('http://'+process.env.RUNNABLE_DOCKER)
+      nock('http://'+process.env.RUNNABLE_DOCKER, {allowUnmocked: true})
         .post('/containers/123456/kill?signal=SIGINT')
         .reply(204);
 
@@ -33,14 +38,22 @@ lab.experiment('signal-client.js', function () {
       });
     });
   });
-  lab.experiment('detachHostFromContainer', function () {
-    it('should return 200 statusCode', function(done) {
-      var signal = new Signal(1,1);
-      signal.detachHostFromContainer(1,1,123456, function(err, res) {
-        if (err) { return done(err); }
-        expect(res.statusCode).to.equal(200);
-        done();
-      });
+  lab.experiment('should error if invalid params', function () {
+    it('should throw if no host', function(done) {
+      try {
+        new Signal(null,1);
+      } catch (err) {
+        return done();
+      }
+      done(new Error('should have errored'));
+    });
+    it('should throw if no port', function(done) {
+      try {
+        new Signal(1,null);
+      } catch (err) {
+        return done();
+      }
+      done(new Error('should have errored'));
     });
   });
 });
