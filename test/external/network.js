d89295f66920b9da9bed430348388212da8d6121
AnandkumarPatel <anandkumarpatel@gmail.com>
2015-10-15 10:36:43 -0700

    fix test, restore stub

--- test/external/network.js
+++ test/external/network.js
@@ -2,6 +2,7 @@
 
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
+var afterEach = lab.afterEach;
 var beforeEach = lab.beforeEach;
 var after = lab.after;
 var expect = require('code').expect;
@@ -24,10 +25,18 @@ lab.experiment('network.js unit test', function () {
     done();
   });
   lab.experiment('valid', function () {
+    beforeEach(function(done) {
+      sinon.stub(childProcess, 'exec');
+      done();
+    });
+    afterEach(function(done) {
+      childProcess.exec.restore();
+      done();
+    });
     lab.it('should attach with correct args', function(done) {
       var testContainer = '1235123512365';
+      childProcess.exec.yields();
       var network = new Network();
-      sinon.stub(childProcess, 'exec').yields();
       network.attach(testContainer, function () {
         expect(childProcess.exec.withArgs(
           process.env.RUNNABLE_WEAVE_PATH +
