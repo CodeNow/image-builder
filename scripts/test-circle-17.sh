6fae1af300b1d2fae12f05afa3f3c9a866d52961
Anandkumar Patel <anand@runnable.com>
2016-02-29 13:01:22 -0800

    find correct container

--- scripts/test-circle-17.sh
+++ scripts/test-circle-17.sh
@@ -2,14 +2,13 @@
 # timeout test
 set -e
 
-test_num="16"
+test_num="17"
 full_repo="bkendall/flaming-octo-nemesis"
 
 mkdir -p ./test-"$test_num"/"$full_repo"
 
 build_log=$(mktemp /tmp/log.XXXX)
-
-# build should timeout
+# build should not timeout
 docker run \
   -e RUNNABLE_AWS_ACCESS_KEY="$AWS_ACCESS_KEY" \
   -e RUNNABLE_AWS_SECRET_KEY="$AWS_SECRET_KEY" \
@@ -23,14 +22,10 @@ docker run \
   -e RUNNABLE_DOCKER="tcp://$(cat DOCKER_IP):5354" \
   -e RUNNABLE_DOCKERTAG='test-built-image' \
   -e RUNNABLE_DOCKER_BUILDOPTIONS='' \
-  -e RUNNABLE_BUILD_LINE_TIMEOUT_MS=1 \
+  -e RUNNABLE_BUILD_LINE_TIMEOUT_MS=999999999 \
   -v `pwd`/test-"$test_num":/cache:rw  \
-  test-image-builder | tee $build_log
+  test-image-builder
 
+# should exit successfully
 exit_code=`docker wait $(docker ps -a -n=1 --no-trunc | grep -v 'CONTAINER' | awk '{print $1}')`
-test "$exit_code" = "124" || (echo "should have exit code 124 " && false)
-
-# should print timeout
-grep -vqE "Runnable: build timeout" "$build_log" || (echo "should have printed build timeout" && false)
-
-
+test "$exit_code" = "0" || (echo "should have exit code 0 " && false)
