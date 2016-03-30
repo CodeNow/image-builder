1ce649f405104bcf67feadc12731468aeb29c959
Anandkumar Patel <anand@runnable.com>
2016-03-30 16:30:13 -0700

    add functional test

---
+++ scripts/test-circle-18.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# build root repo dockerfile
+set -e
+
+build_log=$(mktemp /tmp/log.XXXX)
+
+docker run \
+  -e RUNNABLE_AWS_ACCESS_KEY="$AWS_ACCESS_KEY" \
+  -e RUNNABLE_AWS_SECRET_KEY="$AWS_SECRET_KEY" \
+  -e RUNNABLE_FILES_BUCKET='runnable.image-builder' \
+  -e RUNNABLE_PREFIX='' \
+  -e RUNNABLE_FILES='{ "Dockerfile": "FtqXp6T2nGUU2tlqizN1NgzBRHJ2YFyd" }' \
+  -e RUNNABLE_KEYS_BUCKET='runnable.image-builder' \
+  -e RUNNABLE_DEPLOYKEY='image-builder.key' \
+  -e RUNNABLE_REPO='git@github.com:Runnable/image-builder-test' \
+  -e RUNNABLE_COMMITISH='master' \
+  -e RUNNABLE_DOCKER="tcp://$(cat DOCKER_IP):5354" \
+  -e RUNNABLE_DOCKERTAG='test-built-image' \
+  -e RUNNABLE_DOCKER_BUILDOPTIONS='' \
+  -e RUNNABLE_BUILD_DOCKERFILE='/Dockerfile' \
+  test-image-builder | tee $build_log
+
+# should have printed root repo dockerfile
+grep -vqE "root repo dockerfile" "$build_log" || (echo "should have printed root repo dockerfile" && false)
