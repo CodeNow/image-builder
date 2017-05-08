8f0344e3164f930b34fd92b9bbedb083c2b8735f
Anton Podviaznikov <anton@podviaznikov.com>
2017-05-08 15:30:09 -0400

    more unit tests

--- test/external/vault.js
+++ test/external/vault.js
@@ -8,21 +8,21 @@ const sinon = require('sinon')
 const vault = require('../../lib/external/vault')
 
 lab.experiment('vault.js unit test', () => {
+  lab.beforeEach((done) => {
+    sinon.stub(fs, 'readFileSync').returns('vault-token\n ')
+    process.env.RUNNABLE_VAULT_TOKEN_FILE_PATH = '/some-path'
+    process.env.RUNNABLE_VAULT_ENDPOINT = 'endpoint-for-vault'
+    process.env.RUNNABLE_ORG_ID = 111
+    done()
+  })
+  lab.afterEach((done) => {
+    fs.readFileSync.restore()
+    delete process.env.RUNNABLE_VAULT_TOKEN_FILE_PATH
+    delete process.env.RUNNABLE_VAULT_ENDPOINT
+    delete process.env.RUNNABLE_ORG_ID
+    done()
+  })
   lab.experiment('valid', () => {
-    lab.beforeEach((done) => {
-      sinon.stub(fs, 'readFileSync').returns('vault-token\n ')
-      process.env.RUNNABLE_VAULT_TOKEN_FILE_PATH = '/some-path'
-      process.env.RUNNABLE_VAULT_ENDPOINT = 'endpoint-for-vault'
-      process.env.RUNNABLE_ORG_ID = 111
-      done()
-    })
-    lab.afterEach((done) => {
-      fs.readFileSync.restore()
-      delete process.env.RUNNABLE_VAULT_TOKEN_FILE_PATH
-      delete process.env.RUNNABLE_VAULT_ENDPOINT
-      delete process.env.RUNNABLE_ORG_ID
-      done()
-    })
     lab.it('should be initialized', (done) => {
       const vaultInstance = new vault._VaultManager()
       expect(vaultInstance._vault).to.exist()
@@ -45,33 +45,19 @@ lab.experiment('vault.js unit test', () => {
       done()
     })
   })
-  // lab.experiment('invalid', function () {
-  //   lab.it('should throw if RUNNABLE_DOCKER not set', function(done) {
-  //     try {
-  //       docker()
-  //     } catch (err) {
-  //       return done()
-  //     }
-  //     done(new Error('should have thrown'))
-  //   })
-  //   lab.it('should throw if RUNNABLE_DOCKER has invalid input', function(done) {
-  //     ['google',
-  //     'unix',
-  //     'unix:/bad/place',
-  //     'http://',
-  //     'http://google',
-  //     'http://:235',
-  //     ':235',
-  //     'google:123'].forEach(function(testItem) {
-  //       process.env.RUNNABLE_DOCKER = testItem
-  //       try {
-  //         docker()
-  //       } catch (err) {
-  //         return
-  //       }
-  //       console.log(testItem)
-  //     })
-  //     done()
-  //   })
-  // })
+  lab.experiment('invalid', function () {
+    lab.it('should not be initialized if token path was not set', (done) => {
+      delete process.env.RUNNABLE_VAULT_TOKEN_FILE_PATH
+      const vaultInstance = new vault._VaultManager()
+      expect(vaultInstance._vault).to.not.exist()
+      sinon.assert.notCalled(fs.readFileSync)
+      done()
+    })
+    lab.it('should throw an error when trying to read password from unset vault', (done) => {
+      delete process.env.RUNNABLE_VAULT_TOKEN_FILE_PATH
+      const vaultInstance = new vault._VaultManager()
+      expect(vaultInstance.readRegistryPassword).to.throw(Error, /Vault was not configured/)
+      done()
+    })
+  })
 })
